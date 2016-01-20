70700b0fcc99a002a7818a2d1d9fead413695eaf
Debashish Sanyal <debashishsanyal90@gmail.com>
2016-01-20 01:14:34 -0500

    Fine tuned the code.

--- hw0_dhs2143.py
+++ hw0_dhs2143.py
@@ -1,16 +1,17 @@
-#Hw0 Intro to Databse w111 - Evan Jones
+#HW0, Intro to Database w111 - Evan Jones
 #Due date 01/21/2016
+#UNI dhs2143
+
 import csv
 
 count = 0
 string = "single,malt,scotch"
-rows = 0
+
 with open('iowa-liquor-sample.csv', 'rb') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
+    read_file = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in read_file:
         temp1 = ','.join(row).lower()
         if string in temp1:
             count += 1
-        rows += 1
 
-print str(count) + " 'single malt scotch' found."
+print str(count) + " records contain the phrase 'single malt scotch'."
